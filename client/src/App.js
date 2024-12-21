896f0ba556bf35a14c26bd66fe5175a00352f663
CamHudson808 <hudsonc808@gmail.com>
2024-12-20 22:50:06 -0600

    added client and server

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React, {useEffect, useState} from 'react'
+ 
+function App() {
+
+  const [data, setData] = useState([{}]);
+
+  useEffect(() = > {
+    fetch("")
+  })
+
+  return (
+    <div>
+      
+
+    </div>
+  )
+}
+
+export default App;

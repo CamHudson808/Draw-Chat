b7b869a500e7f9d379ef81818c4b9c27bf2c310f
CamHudson808 <hudsonc808@gmail.com>
2025-09-03 12:24:37 -0500

    Fixed overflow y scroll error

--- client_server_src/client/src/supabase_config/supabaseClient.ts
+++ client_server_src/client/src/supabase_config/supabaseClient.ts
@@ -4,6 +4,7 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
+
 if(!supabaseUrl) {
     throw Error("Supabase url is undefined");
 }
